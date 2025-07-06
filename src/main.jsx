d27232c7a1ed8ca52700eca14fe30716acfebee4
masseycreates <massey@fullcolorcreative.com>
2025-07-06 15:17:02 -0400

    new app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LCv2AdvancedLotterySystem from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <LCv2AdvancedLotterySystem />
+  </React.StrictMode>,
+)
